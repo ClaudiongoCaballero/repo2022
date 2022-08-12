f8ae1ec02858d5ea46836baf5bc4ec770b1d0da4
ClaudiongoCaballero <89614906+ClaudiongoCaballero@users.noreply.github.com>
2022-08-12 01:37:44 -0300

    Update server.js

--- desafio5pug/server.js
+++ desafio5pug/server.js
@@ -2,7 +2,7 @@
 const  { Product } =  require('./product.js')
 const product = new Product('./products.json')
 const express = require('express')
-const pug = require('pug');
+const router = express.Router();
 
 const app = express()
 
@@ -10,21 +10,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-/* app.use('/css', express.static(__dirname, 'node_modules/bootstrap/dist/css'))
-app.use('/js', express.static(__dirname, 'node_modules/bootstrap/dist/js'))
-app.use('/js', express.static(__dirname, 'node_modules/jquery/dist')) */
-
 app.set('view engine', 'pug')
 app.set('views', './views')
 
 app.get('/', (req, res) => {
-    //res.sendFile(__dirname + '/public/index.html');
     res.render('home.pug');
 });
 
 app.get('/api/products/', async (req, res) => {    
     const products = await product.getAll()
-    //const hayLista = allProducts.length > 0;
     res.render('products.pug', {
        products
       })
@@ -33,31 +27,27 @@ app.get('/api/products/', async (req, res) => {
 
 app.get('/api/products/:id', async (req, res) => {  
     let id = req.params.id
-    //console.log(id) 
     const findProduct = product.getById(+id)
-    res.send(findProduct);
+    res.send({product: findProduct});
 })
 
-app.post('/api/products/', async (req, res) => {   
-    const allProducts = product.getAll()
-    let newProduct = { ...req.body, id: allProducts.length + 1 };
-    const product = product.save(newProduct) 
-    res.send(product);
+app.post('/api/products', async (req, res) => {   
+    const allProducts = await product.getAll()        
+    const newProduct = {...req.body, id: allProducts.length + 1 }
+    const productNuevo = product.save(newProduct) 
+    res.json(productNuevo);
 })
 
 app.put('/api/products/:id', async (req, res) => {    
-    //console.log(req.body + " - " + req.params.id)
       const edit = product.edit(req.body, +req.params.id) 
-    res.send(edit);
+    res.json(edit);
 })
 
 app.delete('/api/products/:id', async (req, res) => {    
-    //console.log(req.params.id)
     let id = req.params.id
     const deleteProduct = product.deleteById(+id)
     res.send(deleteProduct);
 })
-
 
 
 app.use( (err, req, res, next) => {
